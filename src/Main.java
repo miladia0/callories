530489dd7059835cd3164ba6fcb7cb5dab5d1835
milad <hatehero522@gmail.com>
2021-07-31 17:42:32 +0430

    change the language to english

--- src/Main.java
+++ src/Main.java
@@ -4,13 +4,13 @@ public class Main {
     public static void main(String[] args) {
         Id id= new Id();Calculetor calculetor=new Calculetor();
         Scanner input =new Scanner(System.in);
-        System.out.println("lotfan jesiat khod ra vared konin ");
+        System.out.println("please enter your gender ");
         id.setJensiat(input.nextBoolean());
-        System.out.println("lotfan sen khod ra ared konin");
+        System.out.println("please enter your age");
         id.setSen(input.nextInt());
-        System.out.println("lotfan vazn khod ra vred konin");
+        System.out.println("please enter your weigh");
         id.setVazn(input.nextInt());
-        System.out.println("lotfan ghad khod ra vared konin");
+        System.out.println("please enter your height");
         id.setGhad(input.nextInt());
       calculetor.getreport(id);
 
